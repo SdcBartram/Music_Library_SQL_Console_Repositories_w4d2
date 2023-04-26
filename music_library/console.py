6cdbe8da2ecf9f5dee69e7d029a030a0235ef9ad
Sam <sdcbartram@icloud.com>
2023-04-26 09:40:41 +0100

    further attempts at extensions

--- music_library/console.py
+++ music_library/console.py
@@ -17,22 +17,30 @@ album2 = Album("The Worst Hits", "Alternative", artist2)
 album_repository.save(album2)
 
 ## Delete all Artists / Albums : 
-album_repository.delete_all()
-artist_repository.delete_all()
+# album_repository.delete_all()
+# artist_repository.delete_all()
 
 ## Delete where id matches
-album_repository.delete(5)
-artist_repository.delete(7)
+# album_repository.delete(5)
+# artist_repository.delete(7)
 
 ## Find Artists/Albums by their ID (select)
-result = album_repository.select(11)
-print(result.__dict__)
+# result = album_repository.select(11)
+# print(result.__dict__)
 
 ## To update an Artist/Album
-album1.title = "The Best Hits"
-album_repository.update(album1) 
+# album1.title = "The Best Hits"
+# album_repository.update(album1) 
+## not working ...
+# album1.title = "The Best Hits"
+# album_repository.update(album1) 
+## still not working ...
+album2 = Album("The Best Hits", "Alternative", artist2)
+album_repository.update(album2)
+## still not working ...
 
 ## List all Artists/Albums (select_all)
 albums = album_repository.select_all()
 for album in albums:
     print(album.__dict__)
+
